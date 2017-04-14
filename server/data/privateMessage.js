c9e83f2bbbe92aef948682bcc1111351c13676b9
Yudong Cao <caoyudong@host-j-36.dhcp.stevens-tech.edu>
2017-04-14 17:20:58 -0400

    add getPrivateMessageById

--- server/data/privateMessage.js
+++ server/data/privateMessage.js
@@ -1,9 +1,20 @@
 const data = require("../data");
 const mongoCollections = require("../config/mongoCollections");
-const provateMessage = mongoCollections.provateMessage;
+const privateMessage = mongoCollections.privateMessage;
 const userData = data.users;
 const uuid = require('node-uuid');
 
 let exportedMethods = {
-    
+    getPrivateMessageById(id) {
+        return privateMessage().then((privateMessageCollection) => {
+            return privateMessageCollection.findOne({_id:id}).then((privateMessage) => {
+                if(!privateMessage) {
+                    throw "Private message not found";
+                }
+                return privateMessage;
+            })
+        })
+    }
 }
+
+module.exports = exportedMethods;
