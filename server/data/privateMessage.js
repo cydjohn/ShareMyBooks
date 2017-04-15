167e5b68f92d448d8d89ad6a4028c1fa79a63037
Yudong Cao <caoyudong@yudongs-MBP.home>
2017-04-15 00:13:11 -0400

    privateMessage

--- server/data/privateMessage.js
+++ server/data/privateMessage.js
@@ -12,8 +12,27 @@ let exportedMethods = {
                     throw "Private message not found";
                 }
                 return privateMessage;
+            });
+        });
+    },
+    addPrivateMessage(message) {
+        return privateMessage().then((privateMessageCollection) => {
+            let newMessage = {
+                _id: uuid.v4(),
+                fromUserId: message.fromUserId,
+                toUserId: message.toUserId,
+                messageText: message.messageText,
+                messageTag: message.messageTag,
+                time: new time.Date()
+            };
+            return privateMessageCollection.insertOne(newMessage).then((newMessageInfo) => {
+                return newMessageInfo.insertedId;
+            }).then((newId) => {
+                return this.getPrivateMessageById(newId);
             })
-        })
+        }).catch((e) => {
+            console.log("Error while adding message:", e)
+        });
     }
 }
 
