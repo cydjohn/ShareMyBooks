4244b6cd9c7df0f2e916b409883502ca989f2ad5
Jitendra Purohit <jpurohit@stevens.edu>
2017-04-19 12:56:06 -0400

    time module commented as it is throwing error for windows system
    jitu

--- server/data/book.js
+++ server/data/book.js
@@ -3,7 +3,7 @@ const es = require("../elastic");
 const elasticsearch = es.book;
 const books = mongoCollections.books;
 const uuid = require('node-uuid');
-const time = require('time');
+// const time = require('time');
    
 
 let exportedMethods = {
@@ -28,7 +28,7 @@ let exportedMethods = {
                 Location: book.Location,
                 Description: book.Description,
                 bookPointsValue: book.bookPointsValue,
-                timestampOfUpload: new time.Date(),
+                // timestampOfUpload: new time.Date(),
                 numberOfRequests: 0,
                 visibleBoolean: book.visibleBoolean
             };
