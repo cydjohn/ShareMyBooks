264a6e092cf1565afb27971e00e69970d009c221
qrk18 <qurikagarg@gmail.com>
2017-05-13 00:13:21 -0400

    changes in footer linked in addeda and  names added in alphabetical order

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -16,10 +16,11 @@ export default class Footer extends React.Component {
               </div>
         </div>
         <div className="subfooter">
-          <p>Made with Love (♥) in New York City by</p> <a href="http://www.linkedin.com/in/jitendra-purohit-bb3389107/">@Jitendra</a> 
-          / <a href="https://www.linkedin.com/in/jhansi-kunam-ba162012b/">@Jhansi</a> 
-          / <a href="">@Qurika</a> 
-          / <a href="https://www.linkedin.com/in/janina-grayson-95695064/">@Janina</a> 
+          <p>Made with Love (♥) in New York City by</p>
+            <a href="https://www.linkedin.com/in/janina-grayson-95695064/">@Janina</a> 
+            / <a href="https://www.linkedin.com/in/jhansi-kunam-ba162012b/">@Jhansi</a> 
+           / <a href="http://www.linkedin.com/in/jitendra-purohit-bb3389107/">@Jitendra</a>           
+          / <a href="https://www.linkedin.com/in/qurika-garg/">@Qurika</a>           
           / <a href="https://www.linkedin.com/in/yudong-cao-0a41a484/">@Yudong</a> <br />
           <p>2017 © ShareMyBooks || All rights Reserved.</p>
         </div>
