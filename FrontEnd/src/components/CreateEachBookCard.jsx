8a23250526c771d383579d2cf2532bceeef551c1
Janina Grayson <jgrays01@nyit.edu>
2017-05-12 22:32:58 -0400

    css card fix for totally test

--- FrontEnd/src/components/CreateEachBookCard.jsx
+++ FrontEnd/src/components/CreateEachBookCard.jsx
@@ -19,10 +19,12 @@ export default class CreateEachBookCard extends React.Component {
 
             <div className="col-md-4">
                 <Card>
-                    <CardBlock>
-                        <CardTitle>{this.props.Title}</CardTitle>
-                        <CardSubtitle>{this.props.Author}</CardSubtitle>
-                    </CardBlock>
+                    <div><h1></h1><h2></h2></div>
+                    <h3><CardBlock>
+                        <h4><CardTitle>{this.props.Title}</CardTitle></h4>
+                        <h5><CardSubtitle>{this.props.Author}</CardSubtitle></h5>
+                        
+                    </CardBlock></h3>
                     <img  height="10px" style={{alignSelf: 'center', width: '150px', height: '200px' }} src={`../bookThumbnailImages/${this.props.bookPhotoID1}.png`} alt="Card image cap" />
                     <CardBlock>
                         <CardText>{this.props.Description}</CardText>
